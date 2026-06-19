a88cbfd6bd4b525deb7a5cdb0908bd723701ff0a
Anosh Billimoria <anoshbilimoria@gmail.com>
2026-06-19 12:26:41 +0200

    fix: re-export all public scanner symbols from scanners/__init__.py

--- src/compliance_scan/scanners/__init__.py
+++ src/compliance_scan/scanners/__init__.py
@@ -1,5 +1,20 @@
-from .pii_scanner import scan_pii, PIIMatch
+from .file_identity import identify_file, FileIdentity
+from .pii_scanner import PIIScanner, PIIMatch, scan_pii
+from .secret_scanner import SecretScanner
+from .keyword_scanner import KeywordScanner
+from .anomaly_scanner import AnomalyScanner
 from .language_detect import detect_language
 from .false_positive_filter import is_technical_document
 
-__all__ = ["scan_pii", "PIIMatch", "detect_language", "is_technical_document"]
+__all__ = [
+    "identify_file",
+    "FileIdentity",
+    "PIIScanner",
+    "PIIMatch",
+    "scan_pii",
+    "SecretScanner",
+    "KeywordScanner",
+    "AnomalyScanner",
+    "detect_language",
+    "is_technical_document",
+]
